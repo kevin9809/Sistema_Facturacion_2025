506f581d19a24975740c8e6007f593f5757c2c67
NAVARRO GUIDO ALFREDO ANTONIO <2911492014@mail.utec.edu.sv>
2025-11-03 00:01:29 -0600

    Consultas LINQ en Clientes e Inventario y creación de Login.

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,24 @@ namespace Proyecto_MVC.Models
     public class Productos
     {
         public int ProductoID { get; set; }
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El nombre no debe exceder los 100 caracteres.")]
         public string NombreProducto { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [DataType(DataType.MultilineText)]
         public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que cero.")]
         public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "El stock es obligatorio.")]
+        [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo.")]
         public int Stock { get; set; }
+
+        [Required(ErrorMessage = "La categoría es obligatoria.")]
+        [StringLength(50, ErrorMessage = "La categoría no debe exceder los 50 caracteres.")]
         public string Categoria { get; set; }
     }
 }
---
+++ Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_MVC.Models
+{
+    public class Usuarios
+    {
+        public int ID_Usuario { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Ingres la contraseña")]
+        [DataType(DataType.Password)]
+        public string  Contraseña { get; set; }
+        public string Email { get; set; }
+        public string  Rol { get; set; }
+    }
+}
